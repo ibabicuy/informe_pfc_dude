81f013bb0d7f231d0ed70f4e70fef76cc3a4a4d9
ibabicuy <ibabic.uy@gmail.com>
2018-03-04 21:20:31 -0300

    Arreglo de faltasde ortografía

--- src/compile_refs.py
+++ src/compile_refs.py
@@ -4,7 +4,7 @@ import subprocess, sys
 
 commands = [
     ['pdflatex', sys.argv[1] + '.tex'],
-    ['bibtex', sys.argv[1] + '.aux'],
+    ['biber', sys.argv[1] + '.aux'],
     ['pdflatex', sys.argv[1] + '.tex'],
     ['pdflatex', sys.argv[1] + '.tex']
 ]
